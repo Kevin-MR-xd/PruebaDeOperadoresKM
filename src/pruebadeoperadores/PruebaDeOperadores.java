046dbc0989699f69923e8961bf030bef39b15281
Kevin MoRo <125381456+Kevin-MR-xd@users.noreply.github.com>
2023-02-28 20:59:07 -0600

    Add files via upload

---
+++ src/pruebadeoperadores/PruebaDeOperadores.java
@@ -0,0 +1,32 @@
+package pruebadeoperadores;
+
+/**
+ *
+ * @author El chistes (Kevin MR)
+ * 
+ * Programa pruebaDeOperadores y su flujo asociado. 
+ * 
+ */
+public class PruebaDeOperadores {
+    int x;
+    int y= 2;
+    double z = 3.0;
+    
+   public void operacion () {
+       x = 5;
+        System.out.println("x + y + z = " + (x + y + z));
+        x += y;
+        y++;
+        z--;
+        z *= x;
+        System.out.println("x + y + z = " + (x + y + z));
+   }
+
+    public static void main(String[] args) {
+        PruebaDeOperadores resultado = new PruebaDeOperadores ();
+        resultado.operacion();
+        
+        
+    }
+
+}
